7c9462d05b44348f065de07652749146e3ec36cf
Damian Daranowski <damian.daranowski@stralfors.se>
2024-09-04 10:10:43 +0200

    Update

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -27,14 +27,13 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shapes API V1");
     });
-}
+
 
 app.UseAuthorization();
 
